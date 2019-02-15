8595a48db655596861341a4d8579010a46467aa8
Vivaldi Ibelio Chandra <ibel@verso.digital>
2019-02-15 23:50:12 +0800

    Some properties are now nullable. No idea why API does that.

--- app/src/main/java/com/vivaldich/genderiseapp/model/Gender.kt
+++ app/src/main/java/com/vivaldich/genderiseapp/model/Gender.kt
@@ -2,7 +2,7 @@ package com.vivaldich.genderiseapp.model
 
 data class Gender(
     val name: String,
-    val gender: String,
-    val probability: String,
-    val count: Number
+    val gender: String?,
+    val probability: String?,
+    val count: Number?
 )
