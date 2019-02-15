c9f14d6ce06862c719d39051a4ba5999401eb2be
Vivaldi Ibelio Chandra <ibel@verso.digital>
2019-02-15 23:51:14 +0800

    Pressing enter on editText will now call the API. Also handles the response and error message properly (hopefully).

--- app/src/main/java/com/vivaldich/genderiseapp/MainActivity.kt
+++ app/src/main/java/com/vivaldich/genderiseapp/MainActivity.kt
@@ -7,21 +7,33 @@ import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
+import com.google.gson.Gson
 import com.vivaldich.genderiseapp.model.Gender
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.java.simpleName
 
+    private var mCompositeDisposable: CompositeDisposable? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initDisposable()
         initComponent()
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        mCompositeDisposable?.clear()
+    }
+
     private fun initComponent() {
-        editFirstName.setOnEditorActionListener(TextView.OnEditorActionListener { v, actionId, event ->
+        editFirstName.setOnEditorActionListener(TextView.OnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 retrieveGenderProbability(editFirstName.text.toString())
                 return@OnEditorActionListener true
@@ -42,13 +54,63 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun retrieveGenderProbability(name: String) {
-        Log.i(TAG, name)
+    private fun initDisposable() {
+        mCompositeDisposable = CompositeDisposable();
+    }
 
-        // TODO: call retrofit API here
+    private fun retrieveGenderProbability(name: String) {
+        if (!GeneralUtils.isStringValid(name)) {
+            GeneralUtils.showSnackBar(parentView, "Please enter a valid name")
+            return
+        }
+
+        progressBar.visibility = View.VISIBLE
+
+        mCompositeDisposable?.add(
+            WebApiClient.instance.get(getFirstWord(name))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(this::showGenderAndProbability, this::handleError)
+        )
     }
 
     private fun showGenderAndProbability(gender: Gender) {
-        // TODO: set textView to visible and edit text accordingly
+        if (isGenderValid(gender)) {
+            textGender.text = gender.gender!!.capitalize()
+            textGender.visibility = View.VISIBLE
+        } else {
+            textGender.visibility = View.INVISIBLE
+        }
+
+        textYouAre.text = parseGenderProbability(gender)
+        textYouAre.visibility = View.VISIBLE
+
+        progressBar.visibility = View.GONE
+    }
+
+    private fun parseGenderProbability(gender: Gender): String {
+        if (!GeneralUtils.isStringValid(gender.probability)) {
+            return "Sorry, your gender is indeterminate..."
+        }
+
+        return "There is a ${Math.round(gender.probability!!.toFloat() * 100)}% chance that you are a..."
+    }
+
+    private fun handleError(error: Throwable) {
+        error.printStackTrace()
+
+        progressBar.visibility = View.GONE
+        GeneralUtils.showSnackBar(parentView, error.message)
+    }
+
+    private fun isGenderValid(gender: Gender): Boolean {
+        return GeneralUtils.isStringValid(gender.probability) &&
+                GeneralUtils.isStringValid(gender.gender)
+    }
+
+    private fun getFirstWord(name: String?): String {
+        if (name == null) return ""
+
+        return name.split(" ")[0]
     }
 }
